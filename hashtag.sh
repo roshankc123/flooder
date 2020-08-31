b4ebf2504d30d2c3b25868d8da08fd7624be3722
roshankc123 <kcroshan566@gmail.com>
2020-08-31 19:14:07 +0545

    fix

--- hashtag.sh
+++ hashtag.sh
@@ -24,19 +24,20 @@ do
             post_count=$(($post_count+1))
             echo -e "\e[36mpost reach={$post_count}\e[0m"
             echo $main_id >> components/PSC/profile_id
-            echo "curl $(cat agent) -b cookie $(cat host) https://mbasic.facebook.com/story.php?story_fbid="$story_id"\&id="$main_id" > components/RAC/comment_page" > components/RAC/tmp_line
-            sh components/RAC/tmp_line
-            echo -e "\e[32mcommenting in story id={$story_id}\e[0m"
-            fb_dtsg=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"fb_dtsg\" value=\").*?(?=\")' | uniq)
-            jazoest=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"jazoest\" value=\").*?(?=\")' | uniq)
-            comment_identifier=$(cat components/RAC/comment_page | grep -oP '(?<=comment_logging&amp;).*?(?=&)' | uniq)
-            if [  -n $fb_dtsg ]  && [ $jazoest ] &&  [ -n $comment_identifier ];then
-                for (( c=1; c<=30; c++ ))
-                do  
+            for (( c=1; c<=5; c++ ))
+            do 
+                echo "curl $(cat agent) -b cookie $(cat host) https://mbasic.facebook.com/story.php?story_fbid="$story_id"\&id="$main_id" > components/RAC/comment_page" > components/RAC/tmp_line
+                sh components/RAC/tmp_line
+                echo -e "\e[32mcommenting in story id={$story_id}\e[0m"
+                fb_dtsg=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"fb_dtsg\" value=\").*?(?=\")' | uniq)
+                jazoest=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"jazoest\" value=\").*?(?=\")' | uniq)
+                comment_identifier=$(cat components/RAC/comment_page | grep -oP '(?<=comment_logging&amp;).*?(?=&)' | uniq)
+                if [  -n $fb_dtsg ]  && [ $jazoest ] &&  [ -n $comment_identifier ];then 
                     echo -e "\e[32mfb_dtsg={$fb_dtsg},jazoest={$jazoest},comment_identifier={$comment_identifier}\e[0m"
                     echo "curl -o /dev/null -w '%{http_code}' $(cat agent) -b cookie $(cat host) --request POST --data \"fb_dtsg=$fb_dtsg&jazoest=$jazoest&comment_text=#JusticeForNirmalaPanta\" https://mbasic.facebook.com/a/comment.php?$comment_identifier" > components/RAC/tmp_line
                     cat components/RAC/tmp_line
                     status=$(sh components/RAC/tmp_line)
+                    echo $status
                     if [ $status == 302 ];then
 			            echo -e "\e[32mdone commenting in story id={$story_id}\e[0m"
                         comment_count=$(($comment_count+1))
@@ -44,8 +45,8 @@ do
 		            else
 		                echo -e "\e[31merror commenting in story id={$story_id}\e[0m"
 		            fi
-                done
-            fi
+                fi
+            done
             i=$(($i+1))
 	    done < components/RAC/story_id
         cursor=$null
