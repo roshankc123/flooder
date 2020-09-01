08a3c9892b3c48ba0a40de697e38e6c4481059df
roshankc123 <kcroshan566@gmail.com>
2020-09-01 13:29:55 +0545

    fix

--- hashtag.sh
+++ hashtag.sh
@@ -2,13 +2,14 @@ cursor=""
 post_count=0
 comment_count=0
 i=0
+cookie="cookie1"
 while read hashtag
 do
     echo $cursor > components/RAC/cursor
     while read cursor
     do
         url="https://mbasic.facebook.com/hashtag/$hashtag/?cursor=$cursor"
-        echo "curl $(cat agent) -b cookie $(cat host) $url  > components/RAC/hashtag_page" > components/RAC/tmp_line
+        echo "curl $(cat agent) -b $cookie $(cat host) $url  > components/RAC/hashtag_page" > components/RAC/tmp_line
         sh components/RAC/tmp_line
         cat components/RAC/hashtag_page | grep -oP '(?<=story.php\?story_fbid=).*?(?=&)' | sort | uniq > components/RAC/story_id
         while read story_id
@@ -22,15 +23,19 @@ do
             echo $main_id >> components/PSC/profile_id
             for (( c=1; c<=5; c++ ))
             do 
-                echo "curl $(cat agent) -b cookie $(cat host) https://mbasic.facebook.com/story.php?story_fbid="$story_id"\&id="$main_id" > components/RAC/comment_page" > components/RAC/tmp_line
+                echo "curl $(cat agent) -b $cookie $(cat host) https://mbasic.facebook.com/story.php?story_fbid="$story_id"\&id="$main_id" > components/RAC/comment_page" > components/RAC/tmp_line
                 sh components/RAC/tmp_line
                 echo -e "\e[32mcommenting in story id={$story_id}\e[0m"
                 fb_dtsg=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"fb_dtsg\" value=\").*?(?=\")' | uniq)
                 jazoest=$(cat components/RAC/comment_page | grep -oP '(?<=name=\"jazoest\" value=\").*?(?=\")' | uniq)
                 comment_identifier=$(cat components/RAC/comment_page | grep -oP '(?<=comment_logging&amp;).*?(?=&)' | uniq)
                 if [  -n $fb_dtsg ]  && [ $jazoest ] &&  [ -n $comment_identifier ];then 
+                    # if [ $(($i%100)) == 0 ];then
+                    #     p=$(($p+1))
+                    #     cookie="cookie"$((($p%2)+1))
+                    # fi
                     echo -e "\e[32mfb_dtsg={$fb_dtsg},jazoest={$jazoest},comment_identifier={$comment_identifier}\e[0m"
-                    echo "curl -o /dev/null -w '%{http_code}' $(cat agent) -b cookie $(cat host) --request POST --data \"fb_dtsg=$fb_dtsg&jazoest=$jazoest&comment_text=#JusticeForNirmalaPanta\" https://mbasic.facebook.com/a/comment.php?$comment_identifier" > components/RAC/tmp_line
+                    echo "curl -o /dev/null -w '%{http_code}' $(cat agent) -b $cookie $(cat host) --request POST --data \"fb_dtsg=$fb_dtsg&jazoest=$jazoest&comment_text=#JusticeForNirmalaPanta\" https://mbasic.facebook.com/a/comment.php?$comment_identifier" > components/RAC/tmp_line
                     # cat components/RAC/tmp_line
                     status=$(sh components/RAC/tmp_line)
                     if [ $status == 302 ];then
--- login.sh
+++ login.sh
@@ -1,11 +1,17 @@
-echo " curl $(cat agent) $(cat host) https://mbasic.facebook.com > components/RAC/login_page" > components/RAC/tmp_line
-sh components/RAC/tmp_line
-lsd=$(cat components/RAC/login_page | grep -oP '(?<=lsd" value=").*?(?=")' | uniq)
-jazoest=$(cat components/RAC/login_page | grep -oP '(?<=jazoest" value=").*?(?=")' | uniq)
-m_ts=$(cat components/RAC/login_page | grep -oP '(?<=m_ts" value=").*?(?=")' | uniq)
-li=$(cat components/RAC/login_page | grep -oP '(?<=li" value=").*?(?=")' | uniq)
-try_number=$(cat components/RAC/login_page | grep -oP '(?<=try_number" value=").*?(?=")' | uniq)
-unrecognized_tries=$(cat components/RAC/login_page | grep -oP '(?<=unrecognized_tries" value=").*?(?=")' | uniq)
-echo "$lsd,$jazoest,$m_ts,$li,$try_number,$unrecognized_tries"
-echo "curl -c cookie $(cat agent) $(cat host) --request POST --data \"lsd=$lsd&jazoest=$jazoest&m_ts=$m_ts&li=$li&try_number==$try_number&unrecognized_tries=$unrecognized_tries&email=jessie.flooder.9&pass=iamJessie\" https://mbasic.facebook.com/login/device-based/regular/login/" > components/RAC/tmp_line
-sh components/RAC/tmp_line
+i=1
+while read ids
+do
+    read email pass <<< $ids
+    echo " curl $(cat agent) $(cat host) https://mbasic.facebook.com > components/RAC/login_page" > components/RAC/tmp_line
+    sh components/RAC/tmp_line
+    lsd=$(cat components/RAC/login_page | grep -oP '(?<=lsd" value=").*?(?=")' | uniq)
+    jazoest=$(cat components/RAC/login_page | grep -oP '(?<=jazoest" value=").*?(?=")' | uniq)
+    m_ts=$(cat components/RAC/login_page | grep -oP '(?<=m_ts" value=").*?(?=")' | uniq)
+    li=$(cat components/RAC/login_page | grep -oP '(?<=li" value=").*?(?=")' | uniq)
+    try_number=$(cat components/RAC/login_page | grep -oP '(?<=try_number" value=").*?(?=")' | uniq)
+    unrecognized_tries=$(cat components/RAC/login_page | grep -oP '(?<=unrecognized_tries" value=").*?(?=")' | uniq)
+    echo "$lsd,$jazoest,$m_ts,$li,$try_number,$unrecognized_tries"
+    echo "curl -c cookie$i $(cat agent) $(cat host) --request POST --data \"lsd=$lsd&jazoest=$jazoest&m_ts=$m_ts&li=$li&try_number==$try_number&unrecognized_tries=$unrecognized_tries&email=$email&pass=$pass\" https://mbasic.facebook.com/login/device-based/regular/login/" > components/RAC/tmp_line
+    sh components/RAC/tmp_line
+    i=$(($i+1))
+done < components/PSC/ids
