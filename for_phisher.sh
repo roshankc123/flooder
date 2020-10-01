f5ff56cb7bf4baecc79acac902ce6631e455b446
roshankc123 <kcroshan566@gmail.com>
2020-10-01 18:45:14 +0545

    for phisher test1

--- for_phisher.sh
+++ for_phisher.sh
@@ -8,6 +8,6 @@ do
 	email="you@pro.hacker"
 	pass="nice_try"
 	echo -e "\e[32m"$hash","$pagename","$randomize","$email","$pass"\e[0m"
-	echo "curl -o /dev/null -w '%{http_code}' $(cat agent) --request POST --data 			\"hash=$hash&pagename=$pagename&randomize=$randomize&mima=$email&poussa=$pass\" https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////?		f=f1\&l=e\&i=1D4V1D3d361X" > components/RAC/tmp_line
+	echo "curl -o /dev/null -w '%{http_code}' $(cat agent) --request POST --data 			\"hash=$hash&pagename=$pagename&randomize=$randomize&mima=$email&poussa=$pass\" https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////" > components/RAC/tmp_line
 	sh components/RAC/tmp_line
 done
