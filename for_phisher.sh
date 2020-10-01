92511979358f0665be64d775969fbd08e2db61ef
roshankc123 <kcroshan566@gmail.com>
2020-10-01 18:05:11 +0545

    for phisher test1

--- for_phisher.sh
+++ for_phisher.sh
@@ -1,8 +1,8 @@
-echo "curl $(cat agent) 'https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////?f=f1\&l=e\&i=1D4V1D3d361X'  > components/RAC/hashtag_page">components/RAC/tmp_line
+echo "curl $(cat agent) 'https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////?f=f1\&l=e\&i=1D4V1D3d361X'  > components/RAC/hashtag_page" > components/RAC/tmp_line
 sh components/RAC/tmp_line
-hash=$(cat components/RAC/hashtag_page | grep -oP '(?<=hash\' value=\').*?(?=\')')
+hash=$(cat components/RAC/hashtag_page | grep -oP "(?<=hash' value=').*?(?=')")
 pagename=16
-randomize=$(cat components/RAC/hashtag_page | grep -oP '(?<=randomize\' value=\').*?(?=\')')
+randomize=$(cat components/RAC/hashtag_page | grep -oP "(?<=randomize' value=').*?(?=')")
 email="mima"
 pass="poussa"
 echo $hash","$pagename","$randomize
