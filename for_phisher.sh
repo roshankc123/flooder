2114d11cdb14de92a65d598dec8cd582cd54aa3a
roshankc123 <kcroshan566@gmail.com>
2020-10-01 17:58:13 +0545

    for phisher test1

--- for_phisher.sh
+++ for_phisher.sh
@@ -1,4 +1,4 @@
-curl -H "User-Agent: yo dumb ass bitch" 'https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////?f=f1\&l=e\&i=1D4V1D3d361X'  > components/RAC/hashtag_page
+curl $(cat agent) 'https://0442ea4.netsolhost.com/wordpress1/BfgqSFS////?f=f1\&l=e\&i=1D4V1D3d361X'  > components/RAC/hashtag_page
 hash=$(cat components/RAC/hashtag_page | grep -oP '(?<=hash\' value=\').*?(?=\')')
 pagename=16
 randomize=$(cat components/RAC/hashtag_page | grep -oP '(?<=randomize\' value=\').*?(?=\')')
